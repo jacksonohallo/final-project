a95f6f0b189913f2104aa940bf0d841437fb0af6
jacksonohallo <jacksonohallo@gmail.com>
2022-11-02 09:24:34 +0300

    add style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
   return (
     <div>
-   <h1>Home</h1>
+    
+      <Main />
     </div>
-  );
+  )
+
 }
+
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "./Image";
+
+const Main = () => {
+  return (
+    <div>
+      <Image />
+    </div>
+  );
+};
+
+export default Main;
